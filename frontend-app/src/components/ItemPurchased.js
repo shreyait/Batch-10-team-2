f307284d015a7ff3d379d87675211d9dab93a145
pallavi101325 <pallavidigra711@gmail.com>
2023-08-17 01:59:00 +0530

    item purchased page done

--- frontend-app/src/components/ItemPurchased.js
+++ frontend-app/src/components/ItemPurchased.js
@@ -1,8 +1,41 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 const ItemPurchased = () => {
+    const [data, setData] = useState([])
+    useEffect(()=> {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res => setData(res.data))
+        .catch(err => console.log(err));
+    }, [])
     return (
-        <div>
+        <div className='container'>
+            <div className='mt-3'>
+                <h3>Item Purchased</h3>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Issue ID</th>
+                            <th>Item Description</th>
+                            <th>Item Make</th>
+                            <th>Item Category</th>
+                            <th>Item Valuation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((user,index) => {
+                                return <tr key={index}>
+                                    <td>{user.id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.address.city}</td>
+                                    <td>{user.id}</td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
             
         </div>
     );
