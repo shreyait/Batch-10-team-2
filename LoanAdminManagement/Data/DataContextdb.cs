b548ede3edbd63fdaa248a6ed3eb5db179f1e467
Pranav Somaiah <pranav.somaiah24@gmail.com>
2023-08-17 07:55:53 +0530

    initial commit

---
+++ LoanAdminManagement/Data/DataContextdb.cs
@@ -0,0 +1,15 @@
+﻿using LoanAdminManagement.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoanAdminManagement.Data
+{
+    public class DataContextdb:DbContext
+    {
+        public DataContextdb(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+
+        }
+        public DbSet<Logger>Loggers { get; set; }
+        public  DbSet<Registration>Registrations { get; set; }
+    }
+}
---
+++ LoanAdminManagement/Model/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoanAdminManagement.Model
+{
+    public class Logger
+    {
+       [Key]
+        [Column(TypeName = "Guid")]
+        public Guid Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string Name { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string password { get; set; }
+    }
+}
